121d6c3e93b3668d3f41fac91c56e9934cbd3572
code-lives <79346591+code-lives@users.noreply.github.com>
2021-06-24 17:47:17 +0800

    Update Im.php

--- src/Im.php
+++ src/Im.php
@@ -203,6 +203,8 @@ class Im
 	}
 	/**
 	 * 【功能说明】拉取聊天记录压缩包
+	 * $parm ChatType 消息类型，C2C 表示单发消息 Group 表示群组消息
+	 * $parm MsgTime 需要下载的消息记录的时间段，2015120121表示获取2015年12月1日21:00 - 21:59的消息的下载地址。该字段需精确到小时。每次请求只能获取某天某小时的所有单发或群组消息记录
 	 * @return URL 压缩包下载地址
 	 * @return ExpireTime 过期时间
 	 * @return FileSize 文件大小 GZip 压缩前的文件大小（单位 Byte）
@@ -211,11 +213,14 @@ class Im
 	 * @return GzipMD5 GZip 压缩后的文件大小（单位 Byte）
 	 *
 	 */
-	public function get_history()
+	public function get_history($parm)
 	{
 		$url = $this->set_url('GET_HISTORY');
-		
-		$data = $this->curl_get($url);
+		$data = array(
+			'ChatType' => $parm['ChatType'],
+			'MsgTime' => $parm['MsgTime'],
+		);
+		$data = $this->curl_get($url, json_encode($data));
 		if ($data['ActionStatus'] == "OK" && $data['ErrorCode'] == 0) {
 			return $data['File'];
 		} else {
